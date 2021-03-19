13a5d9e875ecd27a27f6fea192c074e130ecff45
Emma <benko99emma@gmail.com>
2021-03-19 16:50:37 +0100

    create testcase

---
+++ java/src/hu/holyoil/skeleton/testcases/RobotDies.java
@@ -0,0 +1,21 @@
+package hu.holyoil.skeleton.testcases;
+
+import hu.holyoil.skeleton.TestCase;
+
+public class RobotDies extends TestCase {
+
+    @Override
+    public String Name() {
+        return null;
+    }
+
+    @Override
+    protected void load() {
+
+    }
+
+    @Override
+    protected void start() {
+
+    }
+}
