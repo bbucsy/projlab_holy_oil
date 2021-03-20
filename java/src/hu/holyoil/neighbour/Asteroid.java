a1af446f5b996f8831438a25374b069844c5088e
Berényi Richárd <bercsenyirihi@gmail.com>
2021-03-20 19:15:45 +0100

    Settler Fills (#139)
    #139

--- java/src/hu/holyoil/neighbour/Asteroid.java
+++ java/src/hu/holyoil/neighbour/Asteroid.java
@@ -5,6 +5,7 @@ import hu.holyoil.controller.GameController;
 import hu.holyoil.controller.SunController;
 import hu.holyoil.crewmate.AbstractCrewmate;
 import hu.holyoil.crewmate.IStorageCapable;
+import hu.holyoil.crewmate.Settler;
 import hu.holyoil.resource.AbstractBaseResource;
 import hu.holyoil.skeleton.Logger;
 import hu.holyoil.skeleton.TestFramework;
@@ -48,6 +49,17 @@ public class Asteroid implements INeighbour {
         abstractCrewmate.SetOnAsteroid(this);
         Logger.Return();
 
+    }
+
+    /** eredeti változat a szekvenciadiagramokon: SetResource(s: Settler, resource: AbstractBaseResource)
+     *
+     */
+    public void PutResource(Settler s, AbstractBaseResource res) {
+        if (numOfLayersRemaining == 0 && resource == null) {
+            Logger.Log(this, "Putting down resource from settler to asteroid core.");
+            res.ReactToPlace(this, s);
+            return;
+        }
     }
 
     public void SetIsNearbySun(Boolean newIsNearbySun) {
