be75ab292c28fd88817dcf16300bbe028b549a06
Emma <benko99emma@gmail.com>
2021-03-20 20:33:27 +0100

    controllers thicc doccs
    + some other classes too, minor fixes in code: capital letters for method calls

--- java/src/hu/holyoil/collection/BillOfMaterial.java
+++ java/src/hu/holyoil/collection/BillOfMaterial.java
@@ -7,24 +7,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * nyersanyagokat listázó osztály. Nem összekeverendő a PlayerStorage-al.
+ * nyersanyagokat listázó osztály. Nem összekeverendő a PlayerStorage-al. Elsősorban a receptek kezelésére használjuk.
  */
 public class BillOfMaterial {
     /**
-     * 
+     * nyersanyagok listája
      */
     List<AbstractBaseResource> resources;
 
+    /**
+     * paraméter nélküli konstruktor
+     * <p>inicializálja a resources listát (üres)</p>
+     */
     public BillOfMaterial() {
         resources = new ArrayList<>();
     }
 
+    /**
+     * Hozzzáad egy nyersanyagot a billhez
+     * @param abstractBaseResource a hozzáadandó nyersanyag
+     */
     public void AddMaterial(AbstractBaseResource abstractBaseResource){
         Logger.Log(this,"Adding Material: " + Logger.GetName(abstractBaseResource));
         resources.add(abstractBaseResource);
         Logger.Return();
     }
 
+    /**
+     * Visszaadja a felvett nyersanyagok listáját
+     * @return a nyersanyagok listája
+     */
     public List<AbstractBaseResource> GetMaterials() {
         Logger.Log(this,"Returning materials");
         Logger.Return();
