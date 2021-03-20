93111945be99fe5b33e3d0ed66f2612a748fb051
Emma <benko99emma@gmail.com>
2021-03-20 13:21:09 +0100

    created testcase

---
+++ java/src/hu/holyoil/skeleton/testcases/SettlerDies.java
@@ -0,0 +1,20 @@
+package hu.holyoil.skeleton.testcases;
+
+import hu.holyoil.skeleton.TestCase;
+
+public class SettlerDies extends TestCase {
+    @Override
+    public String Name() {
+        return null;
+    }
+
+    @Override
+    protected void load() {
+        
+    }
+
+    @Override
+    protected void start() {
+
+    }
+}
