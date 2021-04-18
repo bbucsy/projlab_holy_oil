46b6b63489a1d301aa92c48d2c1f048afe2bc864
Beni <bucsy.beni@gmail.com>
2021-04-18 16:14:24 +0200

    fix

--- java/run_all.py
+++ java/run_all.py
@@ -8,4 +8,4 @@ for filename in os.listdir('testdir'):
         # print(str(filename))
         if (str(filename).startswith('test')):
             #print("COMMAND:", 'run.bat < ./' + str(file)+ " > " + os.path.join('testdir', str(filename).replace('test', 'auto_res')))
-            os.system('run.bat < .\\' + str(file)+ " > " + os.path.join('testdir', str(filename).replace('test', 'auto_res')))
+            os.system('run.bat < .\\' + str(file)+ " > " + os.path.join('testdir', str(filename).replace('test', 'res')))
