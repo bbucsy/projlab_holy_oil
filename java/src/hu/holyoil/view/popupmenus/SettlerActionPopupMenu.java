79afa3f2be131cd5c5dc2b536c0f9ddd6ad5b63e
Berényi Richárd <bercsenyirihi@gmail.com>
2021-05-04 11:23:33 +0200

    Popupmenus (#313)
    Should be ok but needs to be tested

--- java/src/hu/holyoil/view/popupmenus/SettlerActionPopupMenu.java
+++ java/src/hu/holyoil/view/popupmenus/SettlerActionPopupMenu.java
@@ -3,6 +3,8 @@ package hu.holyoil.view.popupmenus;
 import com.sun.org.apache.xml.internal.security.Init;
 import hu.holyoil.crewmate.Settler;
 import hu.holyoil.neighbour.Asteroid;
+import hu.holyoil.neighbour.INeighbour;
+import hu.holyoil.neighbour.TeleportGate;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -11,62 +13,90 @@ import java.awt.event.MouseEvent;
 
 public class SettlerActionPopupMenu extends AbstractPopupMenu {
     PopupMenu actionPopupMenu;
-    MenuItem mine, drill;
+    MenuItem mine, drill, travel;
 
     @Override
     protected void InitListeners() {
-
     }
 
-    public SettlerActionPopupMenu(Settler settler, MouseEvent e) {
-        //megnézzük hogy a settler mely asteroidon van
-        Asteroid asteroid = settler.GetOnAsteroid();
+    /**
+     *
+     * @param settler: a settler, amelyik a kattintáskor épp játékban van
+     * @param neighbour: az asteroid/teleportgate amire a játékos kattintott
+     * @param e
+     * Egy popupmenüt hoz létre, amely lekezeli a következő eseteket (feltételezve hogy ez jobb klikkel történt, az nem is kerül vizsgálatra):
+     * - a játékos a saját asteroidjára kattintott, és még annak van kérge -> a popupmenüben csak a drill lehetőség jelenik meg
+     * - a játékos a saját asteroidjára kattintott, de annak már nincs kérge de van benne nyersanyag, ekkor a mine lehetőség jelenik meg
+     * - a játékos a saját asteroidjára kattintott, de annak nincs kérge és üreges, ekkor a popupmenu üres
+     * - a játékos egy szomszédos asteroidra/teleportgatera kattintott, akkor a travel here menüpont jelenik meg
+     * Az összes menüelem meghívja a listenerjében a szükséges függvént ami megvalósítja az elvárt funkciót.
+     */
 
+    public SettlerActionPopupMenu(Settler settler, INeighbour neighbour, MouseEvent e) {
         //létrehozzuk a popupmenu-t
         actionPopupMenu = new PopupMenu();
 
-        //amennyiben az asteroidnak még van kérge, csak fúrható
-        if (asteroid.GetLayerCount() > 0) {
-            mine = new MenuItem("mine");
-            mine.addActionListener(new mineListener(asteroid, settler));
-            actionPopupMenu.add(mine);
-        } else {
-            if (asteroid.GetResource() != null) {
-                drill = new MenuItem("drill");
-                drill.addActionListener(new drillListener(asteroid, settler));
-                actionPopupMenu.add(drill);
+        //a drill és mine lehetőség csak akkor bukkanhat fel ha a sajtá asteroidjára kattintott a settler
+        if(settler.GetOnAsteroid().GetId().equals(neighbour.GetId())) {
+            Asteroid asteroid = (Asteroid) neighbour;
+            //amennyiben az asteroidnak még van kérge, csak fúrható
+            if (asteroid.GetLayerCount() > 0) {
+                mine = new MenuItem("mine");
+                mine.addActionListener(new mineListener(settler));
+                actionPopupMenu.add(mine);
+            } else {
+                //amenniyben az asteroidnak nincs kérge és nem üreges a magja akkor bányászható
+                if (asteroid.GetResource() != null) {
+                    drill = new MenuItem("drill");
+                    drill.addActionListener(new drillListener(settler));
+                    actionPopupMenu.add(drill);
+                }
             }
+        }else{
+            travel = new MenuItem("travel here");
+            travel.addActionListener(new moveListener(settler, neighbour));
+            actionPopupMenu.add(travel);
         }
-
     }
 
-    public class mineListener implements ActionListener {
-        Asteroid asteroid;
+    public static class mineListener implements ActionListener {
         Settler settler;
 
-        mineListener(Asteroid a, Settler s) {
-            asteroid = a;
+        public mineListener(Settler s) {
             settler = s;
         }
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            asteroid.ReactToDrill(settler);
+            settler.Mine();
         }
     }
 
-    public class drillListener implements ActionListener {
-        Asteroid asteroid;
+    public static class drillListener implements ActionListener {
         Settler settler;
 
-        drillListener(Asteroid a, Settler s) {
-            asteroid = a;
+        public drillListener(Settler s) {
             settler = s;
         }
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            asteroid.ReactToMineBy(settler, settler.GetStorage());
+            settler.Drill();
+        }
+    }
+
+    public static class moveListener implements ActionListener{
+        INeighbour neighbour;
+        Settler settler;
+
+        public moveListener(Settler s, INeighbour n){
+            settler = s;
+            neighbour = n;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            settler.Move(neighbour);
         }
     }
 }
