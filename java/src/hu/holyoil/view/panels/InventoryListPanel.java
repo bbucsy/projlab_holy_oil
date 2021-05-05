5e5a92385a7c91fb6fde446a5549abc96218a156
Emma <benko99emma@gmail.com>
2021-05-05 13:47:01 +0200

    :memo: add javadocs to InventoryPanel

--- java/src/hu/holyoil/view/panels/InventoryListPanel.java
+++ java/src/hu/holyoil/view/panels/InventoryListPanel.java
@@ -9,52 +9,91 @@ import hu.holyoil.view.IViewComponent;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
+/**
+ * A soron lévő settler tárolóegységét mutatja, és extra műveleteket kínál.
+ * <p>
+ *     Ezek a műveletek:<ul>
+ *         <li>Robot gyártása</li>
+ *         <li>Teleporter gyártása</li>
+ *         <li>Teleporter elhelyezése</li>
+ *         <li>Kiválasztott nyersanyag visszatöltése üres aszteroidába</li>
+ *     </ul>
+ * </p>
+ */
 public class InventoryListPanel extends JPanel implements IViewComponent {
-
+    /**
+     * A soron lévő játékos tárhelye
+     */
     private PlayerStorage storage;
+    /**
+     * A soron lévő játékos
+     */
     private Settler settler;
+    /**
+     * A megjeleníti a nyersanyagok listáját
+     */
     private JList<AbstractBaseResource> inventory;
-    private JLabel tps;
+    /**
+     * A megjelenítendő lista modelje
+     */
     private DefaultListModel<AbstractBaseResource> model;
+    /**
+     * A teleporter számát megjelenítő felirat
+     */
+    private JLabel tps;
+    /**
+     * Egy robot gyártására szolgáló gomb
+     */
     private JButton craftRobot;
+    /**
+     * Egy teleporter pár gyártására szolgáló gomb
+     */
     private JButton craftTp;
+    /**
+     * Egy teleportert helyez el a jelen aszteroidán
+     */
     private JButton placeTp;
+    /**
+     * Lerakja a kiválasztott nyersanyagot a jelen aszteroidára
+     */
     private JButton fill;
 
+    /**
+     * Inicializálja a panelt
+     */
     private void InitComponent() {
         setLayout(new GridBagLayout());
         settler = TurnController.GetInstance().GetSteppingSettler();
         storage = TurnController.GetInstance().GetSteppingSettler().GetStorage();
 
+        //betölti a nyersanyagok listáját
         model = new DefaultListModel<AbstractBaseResource>();
         storage.GetStoredMaterials().forEach(abr -> model.addElement(abr));
         inventory = new JList<>(model);
 
         inventory.setLayoutOrientation(JList.VERTICAL);
         inventory.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        inventory.setBackground(new Color(4,4,13));
-        inventory.setForeground(Color.GREEN);
+        inventory.setBackground(new Color(4,4,13)); //lista háttere (nem lehet átlátszó)
+        inventory.setForeground(Color.GREEN); //szöveg színe
 
         JScrollPane scrollPane = new JScrollPane(inventory);
         scrollPane.setVerticalScrollBar(new JScrollBar());
         scrollPane.setBackground(new Color(4,4,13));
         scrollPane.setPreferredSize(new Dimension(360, 300));
 
-        tps= new JLabel(String.valueOf(storage.GetTeleporterCount()));
         JLabel tpText = new JLabel("Number of TeleportGates: ");
+        tps= new JLabel(String.valueOf(storage.GetTeleporterCount()));
         craftRobot = new JButton("Craft Robot");
         craftTp = new JButton("Craft Teleporter");
         placeTp = new JButton("Place Teleporter");
         fill = new JButton("Place Resource");
-
+        //első két gomb
         JPanel panel1 = new JPanel();
         panel1.setOpaque(false);
         panel1.add(fill);
         panel1.add(craftRobot);
-
+        //teleporter szöveg és alsó két gomb
         JPanel panel2 = new JPanel();
         panel2.setOpaque(false);
         panel2.setLayout(new BoxLayout(panel2, BoxLayout.Y_AXIS));
@@ -84,6 +123,9 @@ public class InventoryListPanel extends JPanel implements IViewComponent {
         setVisible(true);
     }
 
+    /**
+     * Listenereket ad a gombokhoz
+     */
     private void InitListeners() {
         craftRobot.addActionListener(e -> settler.CraftRobot());
         craftTp.addActionListener(e -> settler.CraftTeleportGate());
@@ -94,6 +136,9 @@ public class InventoryListPanel extends JPanel implements IViewComponent {
         });
     }
 
+    /**
+     * Újratölti a körök végén a tárhelyeket.
+     */
     @Override
     public void UpdateComponent() {
         settler = TurnController.GetInstance().GetSteppingSettler();
@@ -104,6 +149,9 @@ public class InventoryListPanel extends JPanel implements IViewComponent {
         invalidate();
     }
 
+    /**
+     * Konstruktor
+     */
     public InventoryListPanel() {
         super();
         InitComponent();
